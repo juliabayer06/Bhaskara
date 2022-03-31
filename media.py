8dfde773684e5e480c0a5384ae3c9193618bd6be
Julia Bayer <juliabayer130@gmail.com>
2022-03-31 11:26:53 -0300

    Projeto v1 de Bhaskara

---
+++ media.py
@@ -0,0 +1,8 @@
+nota1 = int(input('digite sua primeira nota'))
+nota2 = int(input('digite a segunda nota'))
+nota3 = int(input('digite a terceira nota'))
+nota4 = int(input('digite a quarta nota'))
+
+media = (nota1+nota2+nota3+nota4) /4
+
+print('sua media final é {}'.format(media))
